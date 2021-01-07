782fe5fb88b4c61255491976706062d8f5826684
eight20eight@gmail.com <email-address-you-used-at-github@example.com>
2021-01-07 13:26:49 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BlackjackCS.Models;
+using System.Linq;
+
+namespace BlackjackCS
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
